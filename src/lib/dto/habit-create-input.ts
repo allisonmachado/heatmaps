b61e5368624d2cc0b3be2fc639b1df06921853b5
Allison Machado <aallison93@gmail.com>
2022-11-30 02:26:04 +0000

    fix: validate habit creation

--- src/lib/dto/habit-create-input.ts
+++ src/lib/dto/habit-create-input.ts
@@ -1,11 +1,17 @@
-import { IsString } from 'class-validator';
+import { IsIn, IsString, Length, Matches } from 'class-validator';
 
 export class HabitCreateInput {
   @IsString()
+  @Length(1, 245)
   title: string;
 
   @IsString()
+  @Matches(/^([0-9A-F]{3}){1,2}$/i, {
+    message: 'color must be a valid hex representation',
+  })
   color: string;
 
+  @IsString()
+  @IsIn(['binary', 'timer'])
   type: 'binary' | 'timer';
 }
