525acd98c692f14abfaef2202698704e4b1eb9a6
Allison Machado <aallison93@gmail.com>
2022-12-02 12:36:21 +0000

    chore: configure basic swagger endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Heatmaps')
+    .setDescription('Habits tracker application backend API')
+    .setVersion(process.env.npm_package_version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
