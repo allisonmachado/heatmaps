8f284337875851dabef89ee138713b1f118a69a3
Allison Machado <aallison93@gmail.com>
2022-12-11 00:58:54 +0000

    test: auth service password check

--- src/services/auth.service.spec.ts
+++ src/services/auth.service.spec.ts
@@ -28,4 +28,55 @@ describe('AuthService', () => {
 
     expect(result).toBe(null);
   });
+
+  it("should return null if password doesn't match", async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [AuthService, JwtService],
+    })
+      .useMocker((token) => {
+        if (token === UserService) {
+          return {
+            findOne: jest.fn().mockResolvedValue({
+              name: 'John Doe',
+              email: fakeEmail,
+              password: '123',
+            }),
+          };
+        }
+      })
+      .compile();
+
+    service = moduleRef.get<AuthService>(AuthService);
+
+    const result = await service.validateUser(fakeEmail, fakePwd);
+
+    expect(result).toBe(null);
+  });
+
+  it('should return user if password match', async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [AuthService, JwtService],
+    })
+      .useMocker((token) => {
+        if (token === UserService) {
+          return {
+            findOne: jest.fn().mockResolvedValue({
+              name: 'John Doe',
+              email: fakeEmail,
+              password: fakePwd,
+            }),
+          };
+        }
+      })
+      .compile();
+
+    service = moduleRef.get<AuthService>(AuthService);
+
+    const result = await service.validateUser(fakeEmail, fakePwd);
+
+    expect(result).toEqual({
+      name: 'John Doe',
+      email: fakeEmail,
+    });
+  });
 });
