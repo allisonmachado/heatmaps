43aa862480ca7cf92e5188cb36bec1544420f051
Allison Machado <aallison93@gmail.com>
2022-12-11 14:03:09 +0000

    test: habit service create habit-log

--- src/services/habit.service.spec.ts
+++ src/services/habit.service.spec.ts
@@ -1,8 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Prisma } from '@prisma/client';
 import { PrismaConnector } from '../lib/db/prisma.connector';
 import { LogTypes } from '../lib/dto/log-types.dto';
+import { ForbiddenAccess } from '../lib/errors/forbidden-access';
 import { EntityNotFound } from '../lib/errors/habit-not-found';
 import { InvalidType } from '../lib/errors/invalid-type';
+import { UniqueConstraintFail } from '../lib/errors/unique-constraint-fail';
 import { HabitService } from './habit.service';
 
 describe('HabitService', () => {
@@ -41,7 +44,7 @@ describe('HabitService', () => {
     }).rejects.toThrow(EntityNotFound);
   });
 
-  it('should fail if habit does not exist', async () => {
+  it('should fail if the habit type does not match', async () => {
     const fakePrismaConnector = {
       habit: {
         findUnique: jest.fn().mockResolvedValue({
@@ -69,5 +72,108 @@ describe('HabitService', () => {
         LogTypes.Timer,
       );
     }).rejects.toThrow(InvalidType);
+  });
+
+  it('should fail if the habit does not belong to the user', async () => {
+    const fakePrismaConnector = {
+      habit: {
+        findUnique: jest.fn().mockResolvedValue({
+          type: LogTypes.Timer,
+          userId: 7,
+        }),
+      },
+    };
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [HabitService],
+    })
+      .useMocker((token) => {
+        if (token === PrismaConnector) {
+          return fakePrismaConnector;
+        }
+      })
+      .compile();
+
+    const service = moduleRef.get<HabitService>(HabitService);
+
+    expect(async () => {
+      await service.logUserHabit(
+        fakeHabitId,
+        fakeUserId,
+        fakeTimerLogCreateInput,
+        LogTypes.Timer,
+      );
+    }).rejects.toThrow(ForbiddenAccess);
+  });
+
+  it('should create habit log', async () => {
+    const fakePrismaConnector = {
+      habit: {
+        findUnique: jest.fn().mockResolvedValue({
+          type: LogTypes.Timer,
+          userId: fakeUserId,
+        }),
+      },
+      habitLog: {
+        create: jest.fn(),
+      },
+    };
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [HabitService],
+    })
+      .useMocker((token) => {
+        if (token === PrismaConnector) {
+          return fakePrismaConnector;
+        }
+      })
+      .compile();
+
+    const service = moduleRef.get<HabitService>(HabitService);
+
+    await service.logUserHabit(
+      fakeHabitId,
+      fakeUserId,
+      fakeTimerLogCreateInput,
+      LogTypes.Timer,
+    );
+
+    expect(fakePrismaConnector.habitLog.create).toHaveBeenCalled();
+  });
+
+  it('should fail if the habit log already exists', async () => {
+    const fakePrismaConnector = {
+      habit: {
+        findUnique: jest.fn().mockResolvedValue({
+          type: LogTypes.Timer,
+          userId: fakeUserId,
+        }),
+      },
+      habitLog: {
+        create: jest
+          .fn()
+          .mockRejectedValue(
+            new Prisma.PrismaClientKnownRequestError('', 'P2002', '', null),
+          ),
+      },
+    };
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [HabitService],
+    })
+      .useMocker((token) => {
+        if (token === PrismaConnector) {
+          return fakePrismaConnector;
+        }
+      })
+      .compile();
+
+    const service = moduleRef.get<HabitService>(HabitService);
+
+    expect(async () => {
+      await service.logUserHabit(
+        fakeHabitId,
+        fakeUserId,
+        fakeTimerLogCreateInput,
+        LogTypes.Timer,
+      );
+    }).rejects.toThrow(UniqueConstraintFail);
   });
 });
