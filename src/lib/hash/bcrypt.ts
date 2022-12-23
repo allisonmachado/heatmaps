d19cc98035ad95ce6bf4efd43bec651179338eb5
Allison Machado <aallison93@gmail.com>
2022-12-23 21:59:28 +0000

    refactor: simplify bcrypt implementation

--- src/lib/hash/bcrypt.ts
+++ src/lib/hash/bcrypt.ts
@@ -4,13 +4,15 @@ import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class Bcrypt {
+  compare: typeof bcrypt.compare;
+
+  constructor() {
+    this.compare = bcrypt.compare;
+  }
+
   async hash(text: string): Promise<string> {
     const salt = await bcrypt.genSalt(10);
 
     return await bcrypt.hash(text, salt);
-  }
-
-  async compare(string: string, hash: string): Promise<boolean> {
-    return await bcrypt.compare(string, hash);
   }
 }
