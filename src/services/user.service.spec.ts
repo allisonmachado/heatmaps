516fdebbbb444612d2162e47eafbcc1ef65cac52
Allison Machado <aallison93@gmail.com>
2022-12-23 22:19:32 +0000

    feat: assert valid user credentials

--- src/services/user.service.spec.ts
+++ src/services/user.service.spec.ts
@@ -1,19 +1,51 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { User } from '@prisma/client';
 import { PrismaConnector } from '../lib/db/prisma.connector';
+import { InvalidCredentials } from '../lib/errors/invalid-credentials';
+import { Bcrypt } from '../lib/hash/bcrypt';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  let service: UserService;
-
-  beforeEach(async () => {
+  it('should be defined', async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService, PrismaConnector],
+      providers: [UserService, PrismaConnector, Bcrypt],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    const service = module.get<UserService>(UserService);
+
+    expect(service).toBeDefined();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should not pass with invalid password', async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService, PrismaConnector, Bcrypt],
+    }).compile();
+
+    const userService = module.get<UserService>(UserService);
+    const bcrypt = module.get<Bcrypt>(Bcrypt);
+
+    const fakeUser = {
+      password: await bcrypt.hash('123456'),
+    };
+
+    expect(async () => {
+      await userService.assertValidCredentials(fakeUser as User, 'abc');
+    }).rejects.toThrow(InvalidCredentials);
+  });
+
+  it('should pass with a valid password', async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService, PrismaConnector, Bcrypt],
+    }).compile();
+
+    const userService = module.get<UserService>(UserService);
+    const bcrypt = module.get<Bcrypt>(Bcrypt);
+
+    const password = '123456';
+    const fakeUser = {
+      password: await bcrypt.hash(password),
+    };
+
+    await userService.assertValidCredentials(fakeUser as User, password);
   });
 });
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { PrismaConnector } from '../lib/db/prisma.connector';
+import { InvalidCredentials } from '../lib/errors/invalid-credentials';
+import { Bcrypt } from '../lib/hash/bcrypt';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaConnector) {}
+  constructor(private prisma: PrismaConnector, private bcrypt: Bcrypt) {}
 
   async findOne(email: string): Promise<User> {
     return this.prisma.user.findUnique({
@@ -12,5 +14,14 @@ export class UserService {
         email,
       },
     });
+  }
+
+  public async assertValidCredentials(
+    user: User,
+    password: string,
+  ): Promise<void> {
+    const isValidPassword = await this.bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) throw new InvalidCredentials('password', password);
   }
 }
